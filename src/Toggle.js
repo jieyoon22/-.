8c49f53c07a587e82eef153cb7b0778deef87c5b
jieyoon22 <106982796+jieyoon22@users.noreply.github.com>
2022-09-17 02:43:10 +0900

    add:토글색바뀜

--- src/Toggle.js
+++ src/Toggle.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 
 const Toggle = (props) => {
   const [isValid, setIsValid] = useState(true);
-
-  const col = () => {
-    setIsValid(!isValid);
-    console.log(isValid);
-  };
   return (
     <div>
-      <button onClick={col} style={{ color: isValid ? "red" : "blue" }}>
+      <button
+        onClick={() => {
+          setIsValid(!isValid);
+        }}
+        style={{ color: !isValid && "hotPink" }}
+      >
         토글
       </button>
     </div>
